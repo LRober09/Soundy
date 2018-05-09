c253e094d00f313e54c06652f83cb7a66307a832
Lucas Robertson <lucasrobertson0@gmail.com>
2018-05-09 16:04:01 -0700

    Created main package

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,15 @@
+package main;
 import javafx.*;
 import javafx.application.*;
 import javafx.stage.Stage;
 public class Main extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
 		stage.show();
 	}
 
