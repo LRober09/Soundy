3c060453d9d03850f77ea0d9410440e2732eeea4
david <djames@34north.com>
2018-05-09 15:50:29 -0700

    javafx??

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-
-public class Main {
+import javafx.*;
+import javafx.application.*;
+import javafx.stage.Stage;
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test!");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		stage.show();
 	}
 
 }
