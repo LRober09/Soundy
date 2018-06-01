fe8444aab8bf30c6220f0109857dbd6a5e4a8e2e
david <djames@34north.com>
2018-06-01 15:18:35 -0700

    more smells

--- src/model/BoardCreator.java
+++ src/model/BoardCreator.java
@@ -3,28 +3,28 @@ package model;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
+import java.util.List;
 
 public class BoardCreator {
 	public static SoundBoard soundBoard;
 	public static SoundBoard newBoard;
-	public static ArrayList<String[]> pathlist = new ArrayList<String[]>();
+	public static List<String[]> pathlist;
+	private final static String base = "res/soundboards/";
 	
 	private BoardCreator() {
 		
 	}
 
 	public static void saveBoard(String boardname) throws IOException {
-		String boardPath = "res/soundboards/" + boardname;
-		String imagepath = "res/soundboards/" + boardname + "/images";
-		String soundpath = "res/soundboards/" + boardname + "/sounds";
+		
+		String boardPath = base + boardname;
+		String imagepath = base + boardname + "/images";
+		String soundpath = base + boardname + "/sounds";
 
 		File board = new File(boardPath);
 		if (board.exists()) {
 			throw new IOException("Board Aready Exists");
 		} else {
-			// create it
-			// board.mkdirs();
 			Files.createDirectories(board.toPath());
 		}
 		File images = new File(imagepath);
@@ -35,14 +35,11 @@ public class BoardCreator {
 		for (String[] paths : pathlist) {
 
 			String name = getName(paths[0]);
-			System.out.println(name);
 			File originalImage = new File(paths[0]);
 			File originalSound = new File(paths[1]);
 
-			File newImage = new File(imagepath + "/" + name + ".jpg");
-			File newSound = new File(soundpath + "/" + name + ".wav");
-			// newImage.createNewFile();
-			// newSound.createNewFile();
+			File newImage = new File(imagepath, name + ".jpg");
+			File newSound = new File(soundpath, name + ".wav");
 			Files.copy(originalImage.toPath(), newImage.toPath());
 			Files.copy(originalSound.toPath(), newSound.toPath());
 
@@ -57,8 +54,8 @@ public class BoardCreator {
 		String boardName = loc.split("/")[2];
 		String objectName = loc.split("/")[4].split("\\.")[0];
 		String[] paths = new String[2];
-		paths[0] = "res/soundboards/" + boardName + "/images/" + objectName + ".jpg";
-		paths[1] = "res/soundboards/" + boardName + "/sounds/" + objectName + ".wav";
+		paths[0] = base + boardName + "/images/" + objectName + ".jpg";
+		paths[1] = base + boardName + "/sounds/" + objectName + ".wav";
 		return paths;
 	}
 
