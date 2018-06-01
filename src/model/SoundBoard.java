b983ec3c59596fa3552d24a81acbbdcc2fe77f05
Lucas Robertson <lucasrobertson0@gmail.com>
2018-06-01 15:30:27 -0700

    Removed bin file

--- src/model/SoundBoard.java
+++ src/model/SoundBoard.java
@@ -2,17 +2,19 @@ package model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.util.Duration;
 import view.SoundBoardView;
 
 public class SoundBoard {
 	private static String[] defaultList = { "pig", "cow", "chicken", "dog", "horse" };
 	private SoundBoardView view;
-	private ArrayList<SoundInformation> data;
+	private List<SoundInformation> data;
+
+	private static final String SOUNDBOARD_DIRECTORY = "res/soundboards/";
 
 	public SoundBoard(ArrayList<SoundInformation> data) {
 		this.data = data;
@@ -23,17 +25,16 @@ public class SoundBoard {
 		return view;
 	}
 
-	public static ArrayList<SoundInformation> getDefaultList() {
-		/* TODO: put this default somewhere else! */
+	public static List<SoundInformation> getDefaultList() {
 		return listToInfo(defaultList, "farm");
 	}
 
-	public static ArrayList<SoundInformation> listToInfo(String[] list, String name) {
-		ArrayList<SoundInformation> info = new ArrayList<SoundInformation>();
+	public static List<SoundInformation> listToInfo(String[] list, String name) {
+		ArrayList<SoundInformation> info = new ArrayList<>();
 		for (String item : list) {
 
-			info.add(new SoundInformation("res/soundboards/" + name + "/images/" + item + ".jpg",
-					"res/soundboards/" + name + "/sounds/" + item + ".wav"));
+			info.add(new SoundInformation(SOUNDBOARD_DIRECTORY + name + "/images/" + item + ".jpg",
+					SOUNDBOARD_DIRECTORY + name + "/sounds/" + item + ".wav"));
 		}
 		return info;
 	}
@@ -44,12 +45,12 @@ public class SoundBoard {
 		mediaPlayer.play();
 	}
 
-	public ArrayList<SoundInformation> getData() {
+	public List<SoundInformation> getData() {
 		return data;
 	}
 
-	public static ArrayList<SoundInformation> nameToInfo(String name) {
-		File f = new File("res/soundboards/" + name + "/images");
+	public static List<SoundInformation> nameToInfo(String name) {
+		File f = new File(SOUNDBOARD_DIRECTORY + name + "/images");
 		File[] files = f.listFiles();
 		String[] list = new String[files.length];
 		for (int i = 0; i < files.length; i++) {
@@ -58,29 +59,31 @@ public class SoundBoard {
 		return listToInfo(list, name);
 	}
 
-	public static ArrayList<SoundInformation> pathListToInfo(ArrayList<String[]> list) {
-		ArrayList<SoundInformation> info = new ArrayList<SoundInformation>();
+	public static List<SoundInformation> pathListToInfo(List<String[]> list) {
+		ArrayList<SoundInformation> info = new ArrayList<>();
 		for (String[] item : list) {
 
 			info.add(new SoundInformation(item[0], item[1]));
 		}
 		return info;
 	}
+
 	public static boolean done = false;
+
 	public static boolean isDone() {
 		return done;
 	}
+
 	public void playSound(Button b) {
-		System.out.println("playing sound");
 		int i = getView().buttons.indexOf(b);
 		Media media = new Media(new File(data.get(i).sound).toURI().toString());
 		MediaPlayer mediaPlayer = new MediaPlayer(media);
 		mediaPlayer.play();
 	}
+
 	public MediaPlayer getplayer(Button b) {
 		int i = getView().buttons.indexOf(b);
 		Media media = new Media(new File(data.get(i).sound).toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		return mediaPlayer;
+		return new MediaPlayer(media);
 	}
 }
